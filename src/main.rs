db85b9ef58b7bd67ea541d4c38ab1a1b5f70de92
alephshahor <aalvleo@protonmail.com>
2021-12-16 22:29:36 +0100

    Store exit commd in a variable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::io::Write;
 fn main() {
     let mut input = String::new();
+    let EXIT_CMD = "exit";
+
     loop {
         print!("db> ");
         std::io::stdout().flush().unwrap();
         std::io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
+        
+        // TODO: Handle defer error (if possible)
         match &input.trim()[..] {
-            "exit" => { 
+            x if x == EXIT_CMD => { 
                 println!("Exiting the program");
                 break;
             },
